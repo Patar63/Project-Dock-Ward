2a804d5bdcb4b98aedc88b430bd8e36aba000fd5
Patrick <patrick.thompson8@ontariotechu.net>
2021-10-31 14:00:35 -0400

    Shaders initialized and loaded

--- projects/GDW/src/main.cpp
+++ projects/GDW/src/main.cpp
@@ -5,12 +5,100 @@
 #include "NOU/CMeshRenderer.h"
 #include "NOU/Shader.h"
 #include "NOU/GLTFLoader.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <Player.h>
 
 #include "Logging.h"
 
 #include <memory>
+#include <fstream>
+#include <string>
+#include <iostream>
 
 using namespace nou;
+
+
+GLFWwindow* window;
+
+//initiate GLFW and make sure it works
+bool initGLFW() 
+{
+	if (glfwInit() == GLFW_FALSE) 
+	{
+		std::cout << "Failed to Initialize GLFW" << std::endl;
+		return false;
+	}
+
+	//Create a new GLFW window
+	window = glfwCreateWindow(800, 800, "Project Dock Ward", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+
+	return true;
+}
+
+//initiate GLAD and make sure it works
+bool initGLAD() 
+{
+	if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress) == 0) 
+	{
+		std::cout << "Failed to initialize Glad" << std::endl;
+		return false;
+	}
+}
+
+
+GLuint shader_program;
+
+//load in vertex and fragment shaders
+bool loadShaders() 
+{
+	// Read Shaders from file
+	std::string vert_shader_str;
+	std::ifstream vs_stream("vertex_shader.glsl", std::ios::in);
+	if (vs_stream.is_open()) 
+	{
+		std::string Line = "";
+		while (getline(vs_stream, Line))
+			vert_shader_str += "\n" + Line;
+		vs_stream.close();
+	}
+	else 
+	{
+		printf("Could not open vertex shader!!\n");
+		return false;
+	}
+	const char* vs_str = vert_shader_str.c_str();
+
+	std::string frag_shader_str;
+	std::ifstream fs_stream("frag_shader.glsl", std::ios::in);
+	if (fs_stream.is_open()) {
+		std::string Line = "";
+		while (getline(fs_stream, Line))
+			frag_shader_str += "\n" + Line;
+		fs_stream.close();
+	}
+	else {
+		printf("Could not open fragment shader!!\n");
+		return false;
+	}
+	const char* fs_str = frag_shader_str.c_str();
+
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vs_str, NULL);
+	glCompileShader(vs);
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fs_str, NULL);
+	glCompileShader(fs);
+
+	shader_program = glCreateProgram();
+	glAttachShader(shader_program, fs);
+	glAttachShader(shader_program, vs);
+	glLinkProgram(shader_program);
+
+	return true;
+}
+
 
 template <typename T>
 //templated LERP to be used for bullets
@@ -19,7 +107,25 @@ T Lerp(T a, T b, float t)
 	return(1.0f - t) * a + b * t;
 }
 
+//main game loop inside here as well as call all needed shaders
 int main() 
 {
+	//Initialize GLFW
+	if (!initGLFW())
+		return 1;
+
+	//Initialize GLAD
+	if (!initGLAD())
+		return 1;
+
+	///// Game loop /////
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+	}
 	return 0;
 } 
