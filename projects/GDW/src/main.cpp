908323d31a6ddb15bf70c4bb7e244508335dc6ca
Patrick <patrick.thompson8@ontariotechu.net>
2021-10-31 13:50:11 -0400

    Lerp function templated for use in obstacles and the gun

--- projects/GDW/src/main.cpp
+++ projects/GDW/src/main.cpp
@@ -1,1 +1,25 @@
-int main() { return 0; } 
+#include "NOU/App.h"
+#include "NOU/Input.h"
+#include "NOU/Entity.h"
+#include "NOU/CCamera.h"
+#include "NOU/CMeshRenderer.h"
+#include "NOU/Shader.h"
+#include "NOU/GLTFLoader.h"
+
+#include "Logging.h"
+
+#include <memory>
+
+using namespace nou;
+
+template <typename T>
+//templated LERP to be used for bullets
+T Lerp(T a, T b, float t)
+{
+	return(1.0f - t) * a + b * t;
+}
+
+int main() 
+{
+	return 0;
+} 
