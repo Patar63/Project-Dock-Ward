20379a8634f720ad6a9c45452b6ef6297a716374
Patrick <patrick.thompson8@ontariotechu.net>
2022-04-12 20:06:15 -0400

    Framebuffer edited and Post Processing begins

--- projects/GDW/src/Framebuffer.h
+++ projects/GDW/src/Framebuffer.h
@@ -73,6 +73,8 @@ namespace Semi
 		void Reshape(unsigned width, unsigned height);
 		void SetSize(unsigned width, unsigned height);
 
+		virtual void OnWindowResize(const glm::ivec2& oldSize, const glm::ivec2& newSize) {}
+
 		void SetViewport() const;
 
 		void Bind() const;
